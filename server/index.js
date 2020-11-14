4885afbc5e2ad44d076adc47448473c4b2202064
abeeraftab123 <54543768+abeeraftab123@users.noreply.github.com>
2020-11-14 16:39:31 +0530

    First Commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server); //Instance of socketio
+
+io.on('connection', (socket) => {
+    console.log('A new connection');
+
+    socket.on('disconnect', () => {
+        console.log('User had left');
+    })
+    
+});
+
+
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
